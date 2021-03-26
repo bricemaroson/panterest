352fa1970efc3e6214ec0fd9db9a59b98cb0bf10
cerib <bricemaroson2@gmail.com>
2021-03-26 20:51:45 +0300

    Refactoring by adding Timestampable Trait

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,18 @@ class PinsController extends AbstractController
      */
     public function index(PinRepository $pinRepository): Response
     {
-        $pins = $pinRepository->findAll();
+        $pins = $pinRepository->findBy([], ['createdAt'=>'DESC']);
+        //dd($pins);
         return $this->render('pins/index.html.twig',compact('pins'));
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show(Pin $pin): Response
+    {
+        //dd($pin);
+        return $this->render('pins/show.html.twig',compact('pin'));
+    }
+
 }
--- templates/pins/README.md
+++ templates/pins/README.md
@@ -1,3 +1,4 @@
 ## Créer une fonction twig
 [Voir les commandes=>     symfony console]
 [Ajouter une fonction twig (c'est dans src/twig)=>   symfony console make:twig-extension  ]
+[AJouter date de création et date de mise à jour pour la tri ==> symfony console make:entity]
